bd80814f9118f68f74c8f7e0be5076ceb4b89f41
brobin714 <brobin714@gmail.com>
2024-09-04 02:08:35 -0400

    Inserted if then statements for part repository, outsourced part repository, and product repository if count equals to 0 and save methods for each if then statements into BootStrapData.java.
    Updated README.md with the changes to BootStrapData.java

--- README.md
+++ README.md
@@ -61,7 +61,103 @@ D.  Add an “About” page to the application to describe your chosen customer�
 </pre>
 
 E.  Add a sample inventory appropriate for your chosen store to the application. You should have five parts and five products in your sample inventory and should not overwrite existing data in the database.
+<pre>
 
+INSERT - BootStrapData.java, LINES 65-156
+
+        // Clearing repositories for multiple test runs
+        // partRepository.deleteAll();
+        // productRepository.deleteAll();
+        // outsourcedPartRepository.deleteAll();
+
+        if (partRepository.count() == 0) {
+
+            InhousePart dram100 = new InhousePart();
+            dram100.setName("DRAM100");
+            dram100.setPrice(19.99);
+            dram100.setInv(10);
+
+            InhousePart dram200 = new InhousePart();
+            dram200.setName("DRAM200");
+            dram200.setPrice(29.99);
+            dram200.setInv(10);
+
+            InhousePart dram300 = new InhousePart();
+            dram300.setName("DRAM300");
+            dram300.setPrice(39.99);
+            dram300.setInv(10);
+
+            InhousePart dram400 = new InhousePart();
+            dram400.setName("DRAM400");
+            dram400.setPrice(49.99);
+            dram400.setInv(10);
+
+            InhousePart dram500 = new InhousePart();
+            dram500.setName("DRAM500");
+            dram500.setPrice(59.99);
+            dram500.setInv(10);
+
+            partRepository.save(dram100);
+            partRepository.save(dram200);
+            partRepository.save(dram300);
+            partRepository.save(dram400);
+            partRepository.save(dram500);
+        }
+
+        if (outsourcedPartRepository.count() == 0) {
+
+            OutsourcedPart ssd1100 = new OutsourcedPart();
+            ssd1100.setName("ssd1100");
+            ssd1100.setPrice(19.99);
+            ssd1100.setInv(10);
+            ssd1100.setCompanyName("OthelloCorp");
+
+            OutsourcedPart ssd2200 = new OutsourcedPart();
+            ssd2200.setName("ssd2200");
+            ssd2200.setPrice(29.99);
+            ssd2200.setInv(10);
+            ssd2200.setCompanyName("OthelloCorp");
+
+            OutsourcedPart ssd3300 = new OutsourcedPart();
+            ssd3300.setName("ssd3300");
+            ssd3300.setPrice(39.99);
+            ssd3300.setInv(10);
+            ssd3300.setCompanyName("OthelloCorp");
+
+            OutsourcedPart ssd4400 = new OutsourcedPart();
+            ssd4400.setName("ssd4400");
+            ssd4400.setPrice(49.99);
+            ssd4400.setInv(10);
+            ssd4400.setCompanyName("OthelloCorp");
+
+            OutsourcedPart ssd5500 = new OutsourcedPart();
+            ssd5500.setName("ssd5500");
+           ssd5500.setPrice(59.99);
+           ssd5500.setInv(10);
+           ssd5500.setCompanyName("OthelloCorp");
+
+            outsourcedPartRepository.save(ssd1100);
+            outsourcedPartRepository.save(ssd2200);
+            outsourcedPartRepository.save(ssd3300);
+            outsourcedPartRepository.save(ssd4400);
+            outsourcedPartRepository.save(ssd5500);
+        }
+
+        if (productRepository.count() == 0 ) {
+
+            Product rx710 = new Product("FX710", 199.99, 15);
+            Product rx720 = new Product("FX720", 299.99, 15);
+            Product rx730 = new Product("FX730", 399.99, 15);
+            Product rx740 = new Product("FX740", 499.99, 15);
+            Product rx750 = new Product("FX750", 599.99, 15);
+
+            productRepository.save(rx710);
+            productRepository.save(rx720);
+            productRepository.save(rx730);
+            productRepository.save(rx740);
+            productRepository.save(rx750);
+        }
+</pre>
 
 Note: Make sure the sample inventory is added only when both the part and product lists are empty. When adding the sample inventory appropriate for the store, the inventory is stored in a set so duplicate items cannot be added to your products. When duplicate items are added, make a “multi-pack” part.
 
--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,20 +1,15 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
-import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
-import com.example.demo.service.OutsourcedPartService;
-import com.example.demo.service.OutsourcedPartServiceImpl;
-import com.example.demo.service.ProductService;
-import com.example.demo.service.ProductServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  *
@@ -66,6 +61,100 @@ public class BootStrapData implements CommandLineRunner {
         productRepository.save(bicycle);
         productRepository.save(unicycle);
         */
+
+        // Clearing repositories for multiple test runs
+        // partRepository.deleteAll();
+        // productRepository.deleteAll();
+        // outsourcedPartRepository.deleteAll();
+
+        if (partRepository.count() == 0) {
+
+            InhousePart dram100 = new InhousePart();
+            dram100.setName("DRAM100");
+            dram100.setPrice(19.99);
+            dram100.setInv(10);
+
+            InhousePart dram200 = new InhousePart();
+            dram200.setName("DRAM200");
+            dram200.setPrice(29.99);
+            dram200.setInv(10);
+
+            InhousePart dram300 = new InhousePart();
+            dram300.setName("DRAM300");
+            dram300.setPrice(39.99);
+            dram300.setInv(10);
+
+            InhousePart dram400 = new InhousePart();
+            dram400.setName("DRAM400");
+            dram400.setPrice(49.99);
+            dram400.setInv(10);
+
+            InhousePart dram500 = new InhousePart();
+            dram500.setName("DRAM500");
+            dram500.setPrice(59.99);
+            dram500.setInv(10);
+
+            partRepository.save(dram100);
+            partRepository.save(dram200);
+            partRepository.save(dram300);
+            partRepository.save(dram400);
+            partRepository.save(dram500);
+        }
+
+        if (outsourcedPartRepository.count() == 0) {
+
+            OutsourcedPart ssd1100 = new OutsourcedPart();
+            ssd1100.setName("ssd1100");
+            ssd1100.setPrice(19.99);
+            ssd1100.setInv(10);
+            ssd1100.setCompanyName("OthelloCorp");
+
+            OutsourcedPart ssd2200 = new OutsourcedPart();
+            ssd2200.setName("ssd2200");
+            ssd2200.setPrice(29.99);
+            ssd2200.setInv(10);
+            ssd2200.setCompanyName("OthelloCorp");
+
+            OutsourcedPart ssd3300 = new OutsourcedPart();
+            ssd3300.setName("ssd3300");
+            ssd3300.setPrice(39.99);
+            ssd3300.setInv(10);
+            ssd3300.setCompanyName("OthelloCorp");
+
+            OutsourcedPart ssd4400 = new OutsourcedPart();
+            ssd4400.setName("ssd4400");
+            ssd4400.setPrice(49.99);
+            ssd4400.setInv(10);
+            ssd4400.setCompanyName("OthelloCorp");
+
+            OutsourcedPart ssd5500 = new OutsourcedPart();
+            ssd5500.setName("ssd5500");
+           ssd5500.setPrice(59.99);
+           ssd5500.setInv(10);
+           ssd5500.setCompanyName("OthelloCorp");
+
+            outsourcedPartRepository.save(ssd1100);
+            outsourcedPartRepository.save(ssd2200);
+            outsourcedPartRepository.save(ssd3300);
+            outsourcedPartRepository.save(ssd4400);
+            outsourcedPartRepository.save(ssd5500);
+        }
+
+        if (productRepository.count() == 0 ) {
+
+            Product rx710 = new Product("FX710", 199.99, 15);
+            Product rx720 = new Product("FX720", 299.99, 15);
+            Product rx730 = new Product("FX730", 399.99, 15);
+            Product rx740 = new Product("FX740", 499.99, 15);
+            Product rx750 = new Product("FX750", 599.99, 15);
+
+            productRepository.save(rx710);
+            productRepository.save(rx720);
+            productRepository.save(rx730);
+            productRepository.save(rx740);
+            productRepository.save(rx750);
+        }
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
